c5ae0407754087e82212b5e480e920f32ba5360f
Igor Corradi <igorcorradi.dev@gmail.com>
2016-03-17 12:26:11 -0300

    Clone original images2gif project.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='images2gif',
+      version='0.1',
+      description='',
+      url='',
+      author='',
+      author_email='',
+      license='',
+      packages=['images2gif'],
+      zip_safe=False)
